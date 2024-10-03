c925f988c04530cd258117afba7775b4218368a8
Evhesha <evgenmedvedskii@gmail.com>
2024-10-03 22:07:02 +0300

    Add specific foldres for project

--- EnglishStorageApplication.Server/obj/Debug/net8.0/EnglishStorageApplication.Server.AssemblyInfo.cs
+++ EnglishStorageApplication.Server/obj/Debug/net8.0/EnglishStorageApplication.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca4c4664cd96721abb6bf430067b269919c05e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
