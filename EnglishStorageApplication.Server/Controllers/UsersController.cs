20ea31c0588efebbd762ba9a79e08a282469793d
Evhesha <evgenmedvedskii@gmail.com>
2024-10-07 11:03:00 +0300

    Download EntityFrameworkCore by NuGet. Create user Controllers and Models. Add DbContext cs file

---
+++ EnglishStorageApplication.Server/Controllers/UsersController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EnglishStorageApplication.Server.Services.Interfaces;
+using EnglishStorageApplication.Server.Models;
+using EnglishStorageApplication.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnglishStorageApplication.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase, IUsersController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Реализация методов интерфейса
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ EnglishStorageApplication.Server/Services/Interfaces/IUsersController.cs
@@ -0,0 +1,14 @@
+﻿using EnglishStorageApplication.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishStorageApplication.Server.Services.Interfaces
+{
+    public interface IUsersController
+    {
+        Task<ActionResult<IEnumerable<User>>> GetUsers();
+        Task<ActionResult<User>> GetUser(int id);
+        Task<ActionResult<User>> PostUser(User user);
+        Task<IActionResult> PutUser(int id, User user);
+        Task<IActionResult> DeleteUser(int id);
+    }
+}
