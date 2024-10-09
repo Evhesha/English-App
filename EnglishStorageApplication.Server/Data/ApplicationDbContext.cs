a7c6085e55909f6a607816021b76596fe297cefe
Evhesha <evgenmedvedskii@gmail.com>
2024-10-09 15:40:41 +0300

    check commit

---
+++ EnglishStorageApplication.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EnglishStorageApplication.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnglishStorageApplication.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
--- EnglishStorageApplication.Server/obj/Debug/net8.0/EnglishStorageApplication.Server.AssemblyInfo.cs
+++ EnglishStorageApplication.Server/obj/Debug/net8.0/EnglishStorageApplication.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdacda151a72fe51c6c0c46031612e13cd901904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb57b3059a840df380256ffcde496d58437c9cb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
