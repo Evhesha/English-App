e22fbdb791e3f765ad5af3fb15126dba6d30404a
noemo2004 <kirillz.2004@mail.ru>
2024-10-09 22:19:03 +0300

    side & navbar

--- englishstorageapplication.client/src/Components/Navbar/Navbar.jsx
+++ englishstorageapplication.client/src/Components/Navbar/Navbar.jsx
@@ -3,29 +3,40 @@ import "../Navbar/Navbar.css";
 
 function Navbar({ toggleSidebar, isSidebarOpen }) {
   return (
-    <>
+    <nav className="navbar">
       <div className="container">
-        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-          <div className="d-flex align-items-center">
-            <button className="btn btn-primary me-3" onClick={toggleSidebar}>
-              {isSidebarOpen ? "x" : "≡"}
+        <header className="navbar-content">
+          <div className="navbar-left">
+            <button
+              className={`toggle-btn ${isSidebarOpen ? 'open' : ''}`}
+              onClick={toggleSidebar}
+            >
+              {isSidebarOpen ? "×" : "≡"}
             </button>
-            <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <ul className="nav-list">
               <li>
-                <Link className="nav-link px-2" to={"/home"}>Home</Link>
+                <Link className="nav-link" to={"/home"}>
+                  Home
+                </Link>
               </li>
               <li>
-                <Link className="nav-link px-2" to={"/about-app"}>About</Link>
+                <Link className="nav-link" to={"/about-app"}>
+                  About
+                </Link>
               </li>
             </ul>
           </div>
-          <div className="col-md-3 text-end">
-            <Link type="button" className="btn btn-outline-primary me-2" to={"/login"}>Login</Link>
-            <Link type="button" className="btn btn-primary" to={"/signUp"}>Sign-Up</Link>
+          <div className="navbar-right">
+            <Link className="btn btn-outline" to={"/login"}>
+              Login
+            </Link>
+            <Link className="btn btn-primary" to={"/signUp"}>
+              Sign-Up
+            </Link>
           </div>
         </header>
       </div>
-    </>
+    </nav>
   );
 }
 
