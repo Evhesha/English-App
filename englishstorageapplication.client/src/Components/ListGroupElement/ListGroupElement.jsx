050a46d561e65863096c222bf284c9090666cce3
Evhesha <evgenmedvedskii@gmail.com>
2024-10-17 14:48:41 +0300

    Change list group elements with bootstrap icons. Changes in style.

--- englishstorageapplication.client/src/Components/ListGroupElement/ListGroupElement.jsx
+++ englishstorageapplication.client/src/Components/ListGroupElement/ListGroupElement.jsx
@@ -1,13 +1,16 @@
-function ListGroupElement({name}, {secondName}) {
-
-    return <>
-        <li class="list-group-item">
-            {name} 
-            {secondName} 
-            <button type="button" class="btn btn-primary">Edit</button>
-            <button type="button" class="btn btn-danger">Delete</button>    
+function ListGroupElement({ name, email, password }) {
+    return (
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+            <div> <p>
+            {name} {email} {password}
+                </p>
+            </div>
+            <div>
+                <button type="button" className="btn btn-primary"><i class="bi bi-pencil"></i> Edit</button>
+                <button type="button" className="btn btn-danger"><i class="bi bi-trash3"></i> Delete</button>
+            </div>
         </li>
-    </>
+    );
 }
 
 export default ListGroupElement;
--- englishstorageapplication.client/src/pages/Another/AdminPanel.jsx
+++ englishstorageapplication.client/src/pages/Another/AdminPanel.jsx
@@ -4,12 +4,13 @@ function AdminPanel() {
 
   return (
     <>
+      <i className="bi bi-box-arrow-up-left"></i>
       <h1>Admin Panel</h1>
+      <button className="btn btn-primary">Add user <i class="bi bi-plus-circle"></i></button>
       <div className="list-group">
-        <ListGroupElement name={"Name"}/>
-        <ListGroupElement name={"Name"}/>
-        <ListGroupElement name={"Name"}/>
-
+        <ListGroupElement name={"Name"} email={"evgenii.medvedskii@yandex.by"} password={"genius228"}/>
+        <ListGroupElement name={"Name"} email={"evgenii.medvedskii@yandex.by"} password={"genius228"}/>
+        <ListGroupElement name={"Name"} email={"evgenii.medvedskii@yandex.by"} password={"genius228"}/>
       </div>
     </>
   );
