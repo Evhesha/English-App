3701f3c685cbc267983d787b3acbe6b4a07837bc
Evhesha <evgenmedvedskii@gmail.com>
2024-10-20 21:50:55 +0300

    Add npm style-component. Created link button. New features for ListGroupElements and AdminPanel.

--- englishstorageapplication.client/src/Components/ListGroupElement/ListGroupElement.jsx
+++ englishstorageapplication.client/src/Components/ListGroupElement/ListGroupElement.jsx
@@ -1,9 +1,16 @@
 function ListGroupElement({ name, email, password }) {
     return (
-        <li className="list-group-item d-flex justify-content-between align-items-center">
-            <div> <p>
-            {name} {email} {password}
-                </p>
+        <li className="list-group-item d-flex justify-content-between align-items-center" style={{ border: '2px solid #ccc', borderRadius: '5px', marginBottom: '10px', padding: '10px' }}>
+            <div> 
+            <div style={{ flex: 1 }}> 
+                <p><b>Name: </b> {name}</p>
+            </div>
+            <div style={{ flex: 1 }}>
+                <p><b>Email: </b> {email}</p>
+            </div>
+            <div style={{ flex: 1 }}>
+                <p><b>Password: </b> {password}</p>
+            </div>
             </div>
             <div>
                 <button type="button" className="btn btn-primary"><i class="bi bi-pencil"></i> Edit</button>
--- englishstorageapplication.client/src/pages/Another/AdminPanel.jsx
+++ englishstorageapplication.client/src/pages/Another/AdminPanel.jsx
@@ -1,12 +1,16 @@
 import ListGroupElement from "../../Components/ListGroupElement/ListGroupElement";
+import ToLinkButton from "../../Components/Buttons/ToLinkButton/ToLinkButton";
 
 function AdminPanel() {
 
   return (
     <>
-      <i className="bi bi-box-arrow-up-left"></i>
-      <h1>Admin Panel</h1>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <ToLinkButton link="/home" placeholder={"Home"} />
+        <h1 style={{ marginLeft: '20px' }}>Admin Panel</h1>
+      </div>
       <button className="btn btn-primary">Add user <i class="bi bi-plus-circle"></i></button>
+      <p></p>
       <div className="list-group">
         <ListGroupElement name={"Name"} email={"evgenii.medvedskii@yandex.by"} password={"genius228"}/>
         <ListGroupElement name={"Name"} email={"evgenii.medvedskii@yandex.by"} password={"genius228"}/>
