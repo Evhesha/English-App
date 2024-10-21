0814b8a7697fe10cf7db1022d2ccca60fb38dd2a
Evhesha <evgenmedvedskii@gmail.com>
2024-10-21 10:55:14 +0300

    :red_circle: Delete excess .gitignore file

--- EnglishStorageApplication.Server/obj/Release/net8.0/EnglishStorageApplication.Server.AssemblyInfo.cs
+++ EnglishStorageApplication.Server/obj/Release/net8.0/EnglishStorageApplication.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c86e60a0223568dd146e42396cfe873c54df125a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3701f3c685cbc267983d787b3acbe6b4a07837bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
