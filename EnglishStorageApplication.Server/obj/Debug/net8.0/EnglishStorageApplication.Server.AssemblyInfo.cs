4bb8af786665ccb6bebad92230bde439329bebac
Evhesha <evgenmedvedskii@gmail.com>
2024-10-27 22:49:56 +0300

    Correct Post request in controller. Remake all with Docker files and compose. Correct appsettings.json. Ans other little changes

--- EnglishStorageApplication.Server/obj/Debug/net8.0/EnglishStorageApplication.Server.AssemblyInfo.cs
+++ EnglishStorageApplication.Server/obj/Debug/net8.0/EnglishStorageApplication.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4452e0f9cc6d42dd7aeb50386341605f3b32c1c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9e795491d97bb81cad8a42534082cbd5cc0684")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
