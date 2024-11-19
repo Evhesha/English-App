8309eacbfabfa14624d98444b6e5830bdccc88de
Evhesha <evgenmedvedskii@gmail.com>
2024-11-19 21:54:31 +0300

    Successful complete use tokens for login. Login page

--- EnglishStorageApplication.Server/obj/Debug/net8.0/EnglishStorageApplication.Server.AssemblyInfo.cs
+++ EnglishStorageApplication.Server/obj/Debug/net8.0/EnglishStorageApplication.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79d35498d40e70f0515b317b20464b0a3a979b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75dd1496fa005c01b406b3f7713927c5d14e4951")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishStorageApplication.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
