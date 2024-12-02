cf345a76b225724d4f44ced996021882a674ed3b
noemo2004 <kirillz.2004@mail.ru>
2024-12-02 19:41:00 +0300

    TopicsPage Features

--- englishstorageapplication.client/src/pages/Dictionary/DictionaryTopicsPage/TopicsPage.jsx
+++ englishstorageapplication.client/src/pages/Dictionary/DictionaryTopicsPage/TopicsPage.jsx
@@ -1,5 +1,5 @@
 import Card from "../Card";
-import "../DictionaryTopicsPage/TopicsPage.css"
+import "../DictionaryTopicsPage/TopicsPage.css";
 
 import travelingImage from "../dict-images/traveling.png";
 import cookingImage from "../dict-images/cooking.png";
@@ -9,68 +9,77 @@ import sportImage from "../dict-images/sport.png";
 import workImage from "../dict-images/work.png";
 import programmingPage from "../dict-images/programming.png";
 
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const CardContainer = styled.div`
+  padding-left: 6rem;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3rem; /* Расстояние между карточками */
+  animation: ${fadeIn} 0.6s ease-out;
+`;
+
 function TopicsPage() {
   return (
-    <>
-      <div className="card-container">
-        <Card
-          title={"Traveling"}
-          image={
-            <img src={travelingImage} className="card-img-top" alt="..."></img>
-          }
-          text={"Traveling topic"}
-          link={"/traveling-topic"}
-        />
-        <Card
-          title={"Cooking"}
-          image={
-            <img src={cookingImage} className="card-img-top" alt="..."></img>
-          }
-          text={"Cooking topic"}
-          link={"/cooking-topic"}
-        />
-        <Card
-          title={"Shopping"}
-          image={
-            <img src={shoppingImage} className="card-img-top" alt="..."></img>
-          }
-          text={"Shopping topic"}
-          link={"/shopping-topic"}
-        />
-        <Card
-          title={"Sport"}
-          image={
-            <img src={sportImage} className="card-img-top" alt="..."></img>
-          }
-          text={"Sport topic"}
-          link={"/sport-topic"}
-        />
-        <Card
-          title={"Nature"}
-          image={
-            <img src={natureImage} className="card-img-top" alt="..."></img>
-          }
-          text={"Nature topic"}
-          link={"/nature-topic"}
-        />
-        <Card
-          title={"Work"}
-          image={
-            <img src={workImage} className="card-img-top" alt="..."></img>
-          }
-          text={"Work topic"}
-          link={"/work-topic"}
-        />
-        <Card
-          title={"Programming"}
-          image={
-            <img src={programmingPage} className="card-img-top" alt="..."></img>
-          }
-          text={"Programming topic"}
-          link={"/programming-topic"}
-        />
-      </div>
-    </>
+    <CardContainer>
+      <Card
+        title={"Traveling"}
+        image={
+          <img src={travelingImage} className="card-img-top" alt="..."></img>
+        }
+        text={"Traveling topic"}
+        link={"/traveling-topic"}
+      />
+      <Card
+        title={"Cooking"}
+        image={<img src={cookingImage} className="card-img-top" alt="..."></img>}
+        text={"Cooking topic"}
+        link={"/cooking-topic"}
+      />
+      <Card
+        title={"Shopping"}
+        image={<img src={shoppingImage} className="card-img-top" alt="..."></img>}
+        text={"Shopping topic"}
+        link={"/shopping-topic"}
+      />
+      <Card
+        title={"Sport"}
+        image={<img src={sportImage} className="card-img-top" alt="..."></img>}
+        text={"Sport topic"}
+        link={"/sport-topic"}
+      />
+      <Card
+        title={"Nature"}
+        image={<img src={natureImage} className="card-img-top" alt="..."></img>}
+        text={"Nature topic"}
+        link={"/nature-topic"}
+      />
+      <Card
+        title={"Work"}
+        image={<img src={workImage} className="card-img-top" alt="..."></img>}
+        text={"Work topic"}
+        link={"/work-topic"}
+      />
+      <Card
+        title={"Programming"}
+        image={
+          <img src={programmingPage} className="card-img-top" alt="..."></img>
+        }
+        text={"Programming topic"}
+        link={"/programming-topic"}
+      />
+    </CardContainer>
   );
 }
 
