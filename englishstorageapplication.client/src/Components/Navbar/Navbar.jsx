19cc7310316e56d9d3810349091d0f962ed5465d
Evhesha <evgenmedvedskii@gmail.com>
2024-12-07 18:11:11 +0300

    Dark theme features

--- englishstorageapplication.client/src/Components/Navbar/Navbar.jsx
+++ englishstorageapplication.client/src/Components/Navbar/Navbar.jsx
@@ -2,20 +2,21 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "../Navbar/Navbar.css";
 import axios from "axios";
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import Cookies from "js-cookie";
 import { useTranslation } from "react-i18next";
-import { toast, ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useTheme } from "../ThemeProvider/ThemeProvider";
 
 function Navbar({ toggleSidebar, isSidebarOpen }) {
   const [isAdmin, setIsAdmin] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
+  const { darkMode, toggleTheme } = useTheme();
   const { t } = useTranslation();
 
   const notify = () => {
     toast.success("Theme changed!", {
-      position: "bottom-right"
+      position: "bottom-right",
     });
   };
 
@@ -32,8 +33,6 @@ function Navbar({ toggleSidebar, isSidebarOpen }) {
           } else {
             setIsAdmin(false);
           }
-
-          // Если необходимо, можете добавить обработку данных пользователя
         } catch (error) {
           console.error("Error decoding token:", error);
           setIsAdmin(false);
@@ -46,33 +45,13 @@ function Navbar({ toggleSidebar, isSidebarOpen }) {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-      setDarkMode(true);
-      document.body.classList.add('dark-theme');
-    }
-  }, []);
-
-  const toggleTheme = () => {
-    setDarkMode(!darkMode);
-    if (!darkMode) {
-      document.body.classList.add('dark-theme');
-      localStorage.setItem('theme', 'dark');
-    } else {
-      document.body.classList.remove('dark-theme');
-      localStorage.setItem('theme', 'light');
-    }
-    notify();
-  };
-
   return (
     <nav className="navbar">
       <div className="container">
         <header className="navbar-content">
           <div className="navbar-left">
             <button
-              className={`toggle-btn ${isSidebarOpen ? 'open' : ''}`}
+              className={`toggle-btn ${isSidebarOpen ? "open" : ""}`}
               onClick={toggleSidebar}
             >
               {isSidebarOpen ? "×" : "≡"}
@@ -100,18 +79,32 @@ function Navbar({ toggleSidebar, isSidebarOpen }) {
           <div className="navbar-right">
             <button
               className="theme-toggle-btn"
-              onClick={toggleTheme}
+              onClick={() => {
+                toggleTheme();
+                notify();
+              }}
               title={darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
             >
-              <i className={`bi ${darkMode ? 'bi-sun' : 'bi-moon'}`}></i>
+              <i className={`bi ${darkMode ? "bi-sun" : "bi-moon"}`}></i>
             </button>
             <Link to={"/profile-page"}>
-              <i className="bi bi-person-circle" style={{ fontSize: '2.5rem' }}></i>
+              <i
+                className="bi bi-person-circle"
+                style={{ fontSize: "2.5rem" }}
+              ></i>
             </Link>
-            <Link className="btn btn-outline-primary" to={"/login"} style={{ paddingLeft: "10px", paddingRight: "10px" }}>
+            <Link
+              className="btn btn-outline-primary"
+              to={"/login"}
+              style={{ paddingLeft: "10px", paddingRight: "10px" }}
+            >
               Login <i className="bi bi-box-arrow-in-right"></i>
             </Link>
-            <Link className="btn btn-outline-primary" to={"/signUp"} style={{ paddingLeft: "10px", paddingRight: "10px" }}>
+            <Link
+              className="btn btn-outline-primary"
+              to={"/signUp"}
+              style={{ paddingLeft: "10px", paddingRight: "10px" }}
+            >
               Sign-Up <i className="bi bi-clipboard2-check"></i>
             </Link>
           </div>
