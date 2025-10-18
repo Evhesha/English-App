3f8e5adca2a7a03a32a626e0913d929fcf5aeb7f
Evhesha <evgenmedvedskii@gmail.com>
2025-10-18 14:48:36 +0300

    Feature: get lessons and delete lesson features were added to project.

---
+++ englishstorageapplication.client/src/Components/TeacherPageComp/LessonListElementForUsers.jsx
@@ -0,0 +1,35 @@
+﻿import axios from "axios";
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+function LessonListElementForUsers({ id, name, watchCount, author }) {
+
+
+    return (
+        <li className="list-group-item d-flex justify-content-between align-items-center" style={{ border: '2px solid #ccc', borderRadius: '5px', marginBottom: '10px', padding: '10px' }}>
+            <div>
+                <div style={{ flex: 1 }}>
+                    <p><b>Name: </b> {name}</p>
+                </div>
+                <div style={{ flex: 1 }}>
+                    <p><b>Views: </b> {watchCount}</p>
+                </div>
+                <div style={{ flex: 1 }}>
+                    <p><b>Author: </b> {author}</p>
+                </div>
+            </div>
+            <div>
+                <button type="button" className="btn btn-primary" ><i className="bi bi-book"> </i>Read</button>
+            </div>
+        </li>
+    );
+}
+
+LessonListElementForUsers.propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+};
+
+export default LessonListElementForUsers;
