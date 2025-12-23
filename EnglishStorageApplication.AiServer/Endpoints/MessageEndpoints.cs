424c4f8177b8d99ef32af2effb44658acab0e78f
Evgenii Medvedskii <evgenmedvedskii@gmail.com>
2025-12-23 23:16:58 +0300

    Feature: minimal api endpoints were refactored to special static classes. New changes for messages.

---
+++ EnglishStorageApplication.AiServer/Endpoints/MessageEndpoints.cs
@@ -0,0 +1,32 @@
+using EnglishStorageApplication.AiServer.Abstractions.Services;
+using EnglishStorageApplication.AiServer.DTOs.MessagesDtos;
+using EnglishStorageApplication.AiServer.Models;
+
+namespace EnglishStorageApplication.AiServer.Endpoints;
+
+public static class MessageEndpoints
+{
+    public static void MapMessageEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/chat/{chatId}/messages", async (string chatId, IMessagesService messageService) =>
+        {
+            var chatMessages = await messageService.GetChatMessages(chatId);
+            return Results.Ok(chatMessages);
+        });
+
+        app.MapPost("chat/message/{chatId}", async (
+            string chatId,
+            AddMessageDto messageDto,
+            IMessagesService messageService) =>
+        {
+            var message = new Message
+            {
+                ChatId = messageDto.ChatId,
+                Text = messageDto.Text
+            };
+    
+            await messageService.AddMessage(chatId, message);
+            return Results.Ok();
+        });
+    }
+}
--- EnglishStorageApplication.AiServer/Program.cs
+++ EnglishStorageApplication.AiServer/Program.cs
@@ -1,7 +1,5 @@
-using EnglishStorageApplication.AiServer.Abstractions.Services;
-using EnglishStorageApplication.AiServer.DTOs.ChatsDtos;
+using EnglishStorageApplication.AiServer.Endpoints;
 using EnglishStorageApplication.AiServer.Extensions;
-using EnglishStorageApplication.AiServer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,28 +17,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/user/{userId}/chats", async (string userId, IChatsService chatsService) =>
-{
-    var userChats = await chatsService.GetUserChatsByUserId(userId);
-    return Results.Ok(userChats);
-});
-
-app.MapPost("/chat", async (CreateChatDto CreateChatDto, IChatsService chatsService) =>
-{
-    var chat = new Chat
-    {
-        UserId = CreateChatDto.UserId,
-        Title = CreateChatDto.Title
-    };
-    
-    await chatsService.CreateChat(chat);
-    return Results.Created(); 
-});
-
-app.MapDelete("/chat/{chatId}", async (string chatId, IChatsService chatsService) =>
-{
-    await chatsService.DeleteChat(chatId);
-    return Results.NoContent();
-});
+app.MapChatEndpoints();
+app.MapMessageEndpoints();
 
 app.Run();
