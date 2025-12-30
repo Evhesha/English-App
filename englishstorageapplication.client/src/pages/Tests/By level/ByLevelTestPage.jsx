38bee3a94bc0f9cfd54fe2507295d324a9045471
Evgenii Medvedskii <evgenmedvedskii@gmail.com>
2025-12-30 19:58:57 +0300

    Fix: setting page fixes in activate variable

--- englishstorageapplication.client/src/pages/Tests/By level/ByLevelTestPage.jsx
+++ englishstorageapplication.client/src/pages/Tests/By level/ByLevelTestPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "../styles.css"; // Не забудьте добавить импорт стилей
+import "../styles.css";
 
 function TestCardLink({ link, text, title, icon }) {
   return (
